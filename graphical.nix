4b07dbedb8644af0f14ba7cb0af48ccba99d73ca
Quentin Boyer <quentin.boyer@atos.net>
2021-12-15 10:14:11 +0100

    libreoffice tooling

--- graphical.nix
+++ graphical.nix
@@ -40,6 +40,9 @@
     feh
   ];
 
+  /* environment.pathsToLink = [ "/share/hunspell" "/share/myspell" "/share/hyphen" ];
+    environment.variables.DICPATH = "/run/current-system/sw/share/hunspell:/run/current-system/sw/share/hyphen"; */
+
   home.sessionVariables = {
     BROWSER = "firefox";
   };
