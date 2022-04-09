7647ab47927fb6ec70d8a966e91181d46c424204
Quentin Boyer <quentin@familleboyer.net>
2022-04-09 20:34:48 +0000

    ZeComputa HM + Common information

--- hostconfig/ZeNixComputa/extra_info.nix
+++ hostconfig/ZeNixComputa/extra_info.nix
@@ -1,3 +1,13 @@
 {
   extraInfo.username = "traxys";
+
+  extraInfo.email = "quentin+dev@familleboyer.net";
+
+  extraInfo.inputs = {
+    keyboard = [
+      "12815:20548:Glorious_GMMK_Pro"
+      "12815:20548:Glorious_GMMK_Pro_Consumer_Control"
+    ];
+    #touchpad = "2:7:SynPS/2_Synaptics_TouchPad";
+  };
 }
