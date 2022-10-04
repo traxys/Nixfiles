817d89e4e753872ba883accb94119005b82c3faf
Quentin Boyer <quentin@familleboyer.net>
2022-10-04 18:41:10 +0200

    Introduce ssh picker

--- home.nix
+++ home.nix
@@ -68,11 +68,36 @@ in {
     zoxide = {
       enable = true;
     };
+
+    ssh = {
+      enable = true;
+      matchBlocks = {
+      };
+    };
   };
   home.file = {
     "bin" = {
       source = ./scripts;
       recursive = true;
+    };
+    "bin/ssh-picker" = let
+      ssh_hosts = builtins.attrNames config.programs.ssh.matchBlocks;
+      ssh_specific = builtins.filter (h: !(lib.strings.hasInfix "*" h)) ssh_hosts;
+      ssh_specific_echo = builtins.map (h: "echo ${h}") ssh_specific;
+
+      picker =
+        if config.wm.kind == "sway"
+        then "${pkgs.wofi}/bin/wofi -S dmenu"
+        else "${pkgs.rofi}/bin/rofi -dmenu";
+    in {
+      text = ''
+        #!/usr/bin/env bash
+
+        {
+            ${lib.strings.concatStringsSep "\n" ssh_specific_echo}
+        } | ${picker} | xargs ${config.terminal.command} ssh
+      '';
+      executable = true;
     };
     ".zfunc" = {
       source = ./zfunc;
