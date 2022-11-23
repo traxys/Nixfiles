d424a928c7a16c80b41a8d3f6cf4fed01ba34b00
Quentin Boyer <quentin@familleboyer.net>
2022-11-23 20:43:50 +0100

    Use nixpkgs xdg-ninja

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@
   naersk,
 }: {
   oscclip = callPackage ./oscclip.nix {oscclip-src = sources.oscclip;};
-  xdg-ninja = callPackage ./xdg-ninja.nix {xdg-ninja-src = sources.xdg-ninja;};
   wowup = callPackage ./wowup.nix {};
   simulationcraft = callPackage ./simulationcraft.nix {simulationcraft-src = sources.simulationcraft;};
   proton-ge = callPackage ./proton-ge.nix {proton-ge-src = sources.proton-ge;};
