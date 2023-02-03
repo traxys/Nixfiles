1fd90675e778e310c5350c0ac5be59e100455885
Quentin Boyer <quentin@familleboyer.net>
2023-02-03 18:53:43 +0100

    Fully package wowup

--- pkgs/wowup.nix
+++ pkgs/wowup.nix
@@ -1,11 +1,32 @@
 {
   appimageTools,
   fetchurl,
+  lib,
 }:
-appimageTools.wrapType2 {
-  name = "wowup";
+appimageTools.wrapType2 rec {
+  pname = "wowup-cf";
+  version = "2.9.2";
+
   src = fetchurl {
-    url = "https://github.com/WowUp/WowUp.CF/releases/download/v2.9.2-beta.3/WowUp-CF-2.9.2-beta.3.AppImage";
-    sha256 = "sha256-iijulvH4yjU9vQOyQ0vCBYLR93GGL9Ak/SmVPB+ZDvY=";
+    url = "https://github.com/WowUp/WowUp.CF/releases/download/v${version}/WowUp-CF-${version}.AppImage";
+    sha256 = "sha256-8R6H6ctmVrJgkvthyXGrp1mkthBlr08y1pYIoEkrB7w=";
+  };
+
+  extraInstallCommands = let
+    appimageContents = appimageTools.extractType2 {inherit pname version src;};
+  in ''
+  	mv $out/bin/${pname}-${version} $out/bin/${pname}
+    install -m 444 -D ${appimageContents}/wowup-cf.desktop $out/share/applications/wowup-cf.desktop
+    substituteInPlace $out/share/applications/wowup-cf.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/wowup-cf.png \
+		$out/share/icons/hicolor/512x512/apps/wowup-cf.png
+  '';
+
+  meta = with lib; {
+    homepage = "https://wowup.io/";
+    description = "Tool to install world of warcraft addons";
+    license = licenses.gpl3Plus;
+    platforms = ["x86_64-linux"];
+    maintainers = with maintainers; [traxys];
   };
 }
