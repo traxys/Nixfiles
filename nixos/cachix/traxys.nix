932a3b8c61b70d3d24f7bfbd0c3e27dc6a0c712b
Quentin Boyer <quentin@familleboyer.net>
2023-02-12 15:25:18 +0100

    Configure cachix automatically on NixOS

---
+++ nixos/cachix/traxys.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://traxys.cachix.org"
+      ];
+      trusted-public-keys = [
+        "traxys.cachix.org-1:8Qir8lQJdhzUaw5AE7ICom/IB25wgdheZFxdMln7Qgg="
+      ];
+    };
+  };
+}
