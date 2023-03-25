6c7c7cf1e5415513174a833866eabc194c5e80a3
traxys <quentin@familleboyer.net>
2023-03-26 00:12:32 +0100

    Consolidate a minimal home-manager configuration

--- git/default.nix
+++ git/default.nix
@@ -1,37 +1,6 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{config, ...}: {
   programs.git = {
-    enable = true;
     userName = "traxys";
     userEmail = config.extraInfo.email;
-    delta = {
-      enable = true;
-      options = {
-        line-numbers = true;
-        syntax-theme = "Dracula";
-        plus-style = "auto \"#121bce\"";
-        plus-emph-style = "auto \"#6083eb\"";
-      };
-    };
-    extraConfig = {
-      diff = {
-        algorithm = "histogram";
-      };
-      core = {
-        excludesfile = "${config.home.homeDirectory}/.gitignore";
-      };
-    };
-    aliases = {
-      ri = "rebase -i";
-      amend = "commit --amend";
-    };
-  };
-
-  home.file = {
-    ".gitignore".source = ./gitignore;
   };
 }
