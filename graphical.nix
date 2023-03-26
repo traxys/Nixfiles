a6ba0acbddf4ced15c1bb18f5e2649c0d5e37175
Quentin Boyer <quentin+dev@familleboyer.net>
2023-03-26 17:49:23 +0200

    Cleanup configuration

--- graphical.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-  ];
-
-  /*
-   environment.pathsToLink = [ "/share/hunspell" "/share/myspell" "/share/hyphen" ];
-  environment.variables.DICPATH = "/run/current-system/sw/share/hunspell:/run/current-system/sw/share/hyphen";
-  */
-}
