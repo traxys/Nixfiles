cf627346522f8ba7b587890642fce6ed60219379
Quentin Boyer <quentin.boyer@atos.net>
2023-03-27 14:40:16 +0200

    thinkpad-nixos: fixup localconfig

--- hostconfig/thinkpad-nixos/extra_info.nix
+++ hostconfig/thinkpad-nixos/extra_info.nix
@@ -2,5 +2,8 @@
   extraInfo.email = "quentin.boyer@atos.net";
 
   extraInfo.inputs.keyboard = ["1:1:AT_Translated_Set_2_keyboard"];
+
+  extraInfo.username = "traxys";
+
   extraInfo.inputs.touchpad = "2:7:SynPS/2_Synaptics_TouchPad";
 }
