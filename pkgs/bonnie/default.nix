d98361d222c60a8fc9d4d7b859719d28d4e699ea
traxys <quentin@familleboyer.net>
2023-05-07 18:40:13 +0200

    pkgs: Don't run bonnie tests

--- pkgs/bonnie/default.nix
+++ pkgs/bonnie/default.nix
@@ -1,8 +1,8 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "bonnie";
   version = "0.3.2";
@@ -17,11 +17,13 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-fdkw2QB4n2kbuQtvQ0IagGxEvlnurExTW4UKdwSx93M=";
 
+  doCheck = false;
+
   meta = with lib; {
     description = "Simple, cross-platform, and fast command aliases with superpowers";
     homepage = "https://github.com/arctic-hen7/bonnie";
     changelog = "https://github.com/arctic-hen7/bonnie/blob/${src.rev}/CHANGELOG.md";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [];
   };
 }
