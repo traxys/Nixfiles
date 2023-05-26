0f9c22459d4ea81adce44c78e51712b8020d2580
traxys <quentin@familleboyer.net>
2023-05-26 22:51:07 +0200

    nixos: Use nixvim cachix

---
+++ nixos/cachix/nixvim.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://nixvim.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nixvim.cachix.org-1:8xrm/43sWNaE3sqFYil49+3wO5LqCbS4FHGhMCuPNNA="
+      ];
+    };
+  };
+}
