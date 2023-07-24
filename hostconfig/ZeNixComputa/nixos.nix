f844a999198c7c159cd4c0181410d17c76616ba8
traxys <quentin@familleboyer.net>
2023-07-24 19:52:52 +0200

    ZeComputa: Add microcode updates

--- hostconfig/ZeNixComputa/nixos.nix
+++ hostconfig/ZeNixComputa/nixos.nix
@@ -59,6 +59,8 @@
     ensureDatabases = ["list" "regalade"];
   };
 
+  hardware.cpu.amd.updateMicrocode = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
