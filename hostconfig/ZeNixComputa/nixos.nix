a19346a4509d8104122a9786f689a1354bcaecb0
traxys <quentin@familleboyer.net>
2023-07-27 22:49:38 +0200

    ZeNixComputa: Save derivations for cross system builds

--- hostconfig/ZeNixComputa/nixos.nix
+++ hostconfig/ZeNixComputa/nixos.nix
@@ -61,6 +61,10 @@
 
   hardware.cpu.amd.updateMicrocode = true;
 
+  nix.extraOptions = ''
+    keep-derivations = true
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
