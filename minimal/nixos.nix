4ac5b414c32e57d600cb3c7ccbd8b23e2082c0fe
traxys <quentin@familleboyer.net>
2024-03-30 16:20:46 +0100

    minimal: Mark user as trusted

--- minimal/nixos.nix
+++ minimal/nixos.nix
@@ -65,6 +65,7 @@
     '';
 
     settings = {
+      trusted-users = [config.extraInfo.username];
       auto-optimise-store = true;
       substituters = ["https://nix-gaming.cachix.org"];
       trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
