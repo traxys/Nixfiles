dbc3cdba3977fcf0c6e92dc7c173831323bd447e
traxys <quentin@familleboyer.net>
2024-04-01 18:36:28 +0200

    minus: Add jellyfin

--- hostconfig/minus/hm.nix
+++ hostconfig/minus/hm.nix
@@ -1,6 +1,22 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "${config.extraInfo.username}";
   home.homeDirectory = "/home/${config.extraInfo.username}";
+
+  home.packages = with pkgs; [
+    jellyfin-media-player
+  ];
+
+  home.sessionVariables = {
+    SDL_VIDEODRIVER = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = 1;
+    BROWSER = "firefox";
+  };
 
   home.stateVersion = "23.11";
 }
