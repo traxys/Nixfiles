1ee590353f6b1ba5c806e8325ea90f3e456028a9
traxys <quentin@familleboyer.net>
2024-04-01 19:24:50 +0200

    minus: Allow to setup kdeconnect

--- hostconfig/minus/nixos.nix
+++ hostconfig/minus/nixos.nix
@@ -51,6 +51,8 @@
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "traxys";
 
+  programs.kdeconnect.enable = true;
+
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
