05f76a4a65407daa5f01243165b6ee7a88d8d0a8
traxys <quentin@familleboyer.net>
2024-05-10 10:35:08 +0200

    pkgs: Stop using nvfetcher for warcraft logs

--- pkgs/warcraftlogs.nix
+++ pkgs/warcraftlogs.nix
@@ -1,10 +1,16 @@
 {
-  warcraftlogs-src,
   appimageTools,
   lib,
+  fetchurl,
 }:
 appimageTools.wrapType2 rec {
-  inherit (warcraftlogs-src) pname src version;
+  pname = "warcraftlogs";
+  version = "6.0.2";
+
+  src = fetchurl {
+    url = "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v${version}/Warcraft-Logs-Uploader-${version}.AppImage";
+    hash = "sha256-b1Lt8ssL+Isd/Twv6ef3AK4/BkxWA/TtleameONo/2Q=";
+  };
 
   extraInstallCommands = let
     appimageContents = appimageTools.extractType2 {inherit pname version src;};
@@ -12,7 +18,7 @@ appimageTools.wrapType2 rec {
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
     substituteInPlace $out/share/applications/${pname}.desktop --replace 'Exec=AppRun' 'Exec=${pname}-${version}'
     install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/${pname}.png \
-		$out/share/icons/hicolor/256x256/apps/${pname}png
+      $out/share/icons/hicolor/256x256/apps/${pname}png
   '';
 
   meta = with lib; {
