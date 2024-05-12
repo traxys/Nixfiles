c47a74bb70b4b564c4ae065f02b400b52a535135
Quentin Boyer <quentin.boyer@eviden.com>
2024-05-12 19:15:07 +0200

    pkgs: Update warcraftlogs

--- pkgs/warcraftlogs.nix
+++ pkgs/warcraftlogs.nix
@@ -5,20 +5,20 @@
 }:
 appimageTools.wrapType2 rec {
   pname = "warcraftlogs";
-  version = "6.0.2";
+  version = "8.5.1";
 
   src = fetchurl {
-    url = "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v${version}/Warcraft-Logs-Uploader-${version}.AppImage";
-    hash = "sha256-b1Lt8ssL+Isd/Twv6ef3AK4/BkxWA/TtleameONo/2Q=";
+    url = "https://github.com/RPGLogs/Uploaders-warcraftlogs/releases/download/v${version}/warcraftlogs-v${version}.AppImage";
+    hash = "sha256-jShv1FVOzEMThS7dpWCJp1Eu8exETzJ8wTVz1DZ5xE4=";
   };
 
   extraInstallCommands = let
     appimageContents = appimageTools.extractType2 {inherit pname version src;};
   in ''
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-    substituteInPlace $out/share/applications/${pname}.desktop --replace 'Exec=AppRun' 'Exec=${pname}-${version}'
-    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/${pname}.png \
-      $out/share/icons/hicolor/256x256/apps/${pname}png
+    substituteInPlace $out/share/applications/${pname}.desktop --replace-fail 'Exec=AppRun' 'Exec=${pname}-${version}'
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/${pname}.png \
+      $out/share/icons/hicolor/0x0/apps/${pname}.png
   '';
 
   meta = with lib; {
