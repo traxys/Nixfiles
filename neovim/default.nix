15310363d14224f0d087c2646a473ae5907aff48
Quentin Boyer <quentin+dev@familleboyer.net>
2024-07-25 19:39:39 +0200

    neovim: Split off LSP configuration

--- neovim/default.nix
+++ neovim/default.nix
@@ -3,11 +3,12 @@
   config,
   helpers,
   lib,
-  flake,
   ...
 }:
 {
   imports = [
+    ./lsp.nix
+      
     ./modules/commands.nix
 
     ./plugins/firenvim.nix
@@ -41,36 +42,6 @@
           vim.diagnostic.disable(0)
         end
       '';
-    }
-    {
-      event = [
-        "BufNewFile"
-        "BufRead"
-      ];
-      pattern = [
-        "meson.build"
-        "meson_options.txt"
-        "meson.options"
-      ];
-      callback =
-        let
-          settings = { };
-        in
-        helpers.mkRaw ''
-          function(args)
-            local match = vim.fs.find(
-              {"meson_options.txt", "meson.options", ".git"},
-              {path = args.file, upward = true}
-            )[1]
-            local root_dir = match and vim.fn.fnamemodify(match, ":p:h") or nil
-            vim.lsp.start({
-              name = "mesonlsp",
-              cmd = {"${lib.getExe pkgs.mesonlsp}", "--lsp"},
-              root_dir = root_dir,
-              settings = ${helpers.toLuaObject settings},
-            })
-          end
-        '';
     }
     {
       event = "BufReadPost";
@@ -237,94 +208,10 @@
     keymaps.enable = true;
   };
 
-  plugins.efmls-configs = {
-    enable = true;
-
-    toolPackages.mdformat = pkgs.mdformat.withPlugins (
-      ps: with ps; [
-        # TODO: broken with update of mdformat
-        # mdformat-gfm
-        mdformat-frontmatter
-        mdformat-footnote
-        mdformat-tables
-        mdit-py-plugins
-      ]
-    );
-
-    setup = {
-      sh = {
-        #linter = "shellcheck";
-        formatter = "shfmt";
-      };
-      bash = {
-        #linter = "shellcheck";
-        formatter = "shfmt";
-      };
-      c = {
-        linter = "cppcheck";
-      };
-      markdown = {
-        formatter = [
-          "cbfmt"
-          "mdformat"
-        ];
-      };
-      python = {
-        formatter = "black";
-      };
-      nix = {
-        linter = "statix";
-      };
-      lua = {
-        formatter = "stylua";
-      };
-      html = {
-        formatter = [
-          "prettier"
-          (helpers.mkRaw "djlint_fmt")
-        ];
-      };
-      htmldjango = {
-        formatter = [ (helpers.mkRaw "djlint_fmt") ];
-        linter = "djlint";
-      };
-      json = {
-        formatter = "prettier";
-      };
-      css = {
-        formatter = "prettier";
-      };
-      ts = {
-        formatter = "prettier";
-      };
-      gitcommit = {
-        linter = "gitlint";
-      };
-    };
-  };
-  # plugins.null-ls = {
-  #   enable = true;
-  #   sources = {
-  #     diagnostics = {
-  #       gitlint.enable = true;
-  #     };
-  #     code_actions = {
-  #       shellcheck.enable = true;
-  #       #gitsigns.enable = true;
-  #     };
-  #     formatting = {
-  #       alejandra.enable = true;
-  #     };
-  #   };
-  # };
   plugins.gitsigns.enable = true;
   plugins.gitmessenger.enable = true;
 
   plugins.firenvim.enable = false;
-
-  plugins.luasnip = {
-    enable = true;
-  };
 
   extraConfigLuaPre = ''
     local has_words_before = function()
@@ -333,12 +220,6 @@
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
     local luasnip = require("luasnip")
-
-    local efm_fs = require('efmls-configs.fs')
-    local djlint_fmt = {
-      formatCommand = string.format('%s --reformat ''${INPUT} -', efm_fs.executable('djlint')),
-      formatStdin = true,
-    }
   '';
 
   plugins.cmp = {
@@ -355,32 +236,34 @@
         "<C-d>" = "cmp.mapping.scroll_docs(-4)";
         "<C-f>" = "cmp.mapping.scroll_docs(4)";
         "<C-Space>" = "cmp.mapping.complete()";
-        "<Tab>" = ''
-          cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" })
-        '';
-        "<S-Tab>" = ''
-          cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" })
-        '';
+        "<Tab>" = # lua
+          ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+              -- they way you will only jump inside the snippet region
+              elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              elseif has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+        "<S-Tab>" = # lua
+          ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
         "<Down>" = "cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'})";
         "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'})";
       };
@@ -511,151 +394,7 @@
     };
   };
 
-  plugins.lsp = {
-    enable = true;
-
-    enabledServers = [
-      # {
-      #   name = "groovyls";
-      #   extraOptions = {
-      #     cmd = ["${pkgs.groovy-language-server}/bin/groovy-language-server"];
-      #   };
-      # }
-    ];
-
-    keymaps = {
-      silent = true;
-
-      lspBuf = {
-        "gd" = "definition";
-        "gD" = "declaration";
-        "ca" = "code_action";
-        "ff" = "format";
-        "K" = "hover";
-      };
-    };
-
-    servers = {
-      nixd = {
-        enable = true;
-        settings = {
-          formatting.command = [ (lib.getExe pkgs.nixfmt-rfc-style) ];
-          options =
-            let
-              getFlake = ''(builtins.getFlake "${flake}")'';
-            in
-            {
-              nixos.expr = ''${getFlake}.nixosConfigurations.ZeNixComputa.options'';
-              nixvim.expr = ''${getFlake}.packages.${pkgs.system}.neovimTraxys.options'';
-              home-manager.expr = ''${getFlake}.homeConfigurations."boyerq@thinkpad-nixos".options'';
-            };
-        };
-      };
-      bashls.enable = true;
-      dartls.enable = true;
-      clangd.enable = true;
-      typst-lsp.enable = true;
-      tsserver.enable = true;
-      efm.extraOptions = {
-        init_options = {
-          documentFormatting = true;
-        };
-        settings = {
-          logLevel = 1;
-        };
-      };
-      taplo.enable = true;
-      lemminx.enable = true;
-      ltex = {
-        enable = true;
-        onAttach.function = ''
-          require("ltex_extra").setup{
-            load_langs = { "en-US", "fr-FR" },
-            path = ".ltex",
-          }
-        '';
-        filetypes = [
-          "bib"
-          "gitcommit"
-          "markdown"
-          "org"
-          "plaintex"
-          "rst"
-          "rnoweb"
-          "tex"
-          "pandoc"
-          "typst"
-          #"mail"
-        ];
-      };
-    };
-  };
-
   plugins.typst-vim.enable = true;
-
-  plugins.rustaceanvim = {
-    enable = true;
-
-    settings.server = {
-      default_settings.rust-analyzer = {
-        cargo.features = "all";
-        checkOnSave = true;
-        check.command = "clippy";
-        rustc.source = "discover";
-      };
-    };
-  };
-
-  plugins.lspkind = {
-    enable = true;
-    cmp = {
-      enable = true;
-    };
-  };
-
-  plugins.nvim-lightbulb = {
-    enable = true;
-    settings.autocmd.enabled = true;
-  };
-
-  plugins.lsp_signature = {
-    #enable = true;
-  };
-
-  plugins.inc-rename = {
-    enable = true;
-  };
-
-  plugins.clangd-extensions = {
-    enable = true;
-    enableOffsetEncodingWorkaround = true;
-
-    ast = {
-      roleIcons = {
-        type = "";
-        declaration = "";
-        expression = "";
-        specifier = "";
-        statement = "";
-        templateArgument = "";
-      };
-      kindIcons = {
-        compound = "";
-        recovery = "";
-        translationUnit = "";
-        packExpansion = "";
-        templateTypeParm = "";
-        templateTemplateParm = "";
-        templateParamObject = "";
-      };
-    };
-  };
-
-  # fidget = {
-  #   enable = true;
-  #
-  #   sources.null-ls.ignore = true;
-  # };
 
   plugins.lualine = {
     enable = true;
@@ -688,12 +427,6 @@
     };
   };
 
-  plugins.netman = {
-    enable = false;
-    package = pkgs.vimPlugins.netman-nvim;
-    neoTreeIntegration = true;
-  };
-
   plugins.openscad = {
     enable = true;
     loadSnippets = true;
@@ -704,57 +437,6 @@
     require("luasnip.loaders.from_snipmate").lazy_load()
 
     require("cmp_git").setup({})
-
-    vim.api.nvim_create_user_command("LtexLangChangeLanguage", function(data)
-        local language = data.fargs[1]
-        local bufnr = vim.api.nvim_get_current_buf()
-        local client = vim.lsp.get_active_clients({ bufnr = bufnr, name = 'ltex' })
-        if #client == 0 then
-            vim.notify("No ltex client attached")
-        else
-            client = client[1]
-            client.config.settings = {
-                ltex = {
-                    language = language
-                }
-            }
-            client.notify('workspace/didChangeConfiguration', client.config.settings)
-            vim.notify("Language changed to " .. language)
-        end
-      end, {
-        nargs = 1,
-        force = true,
-    })
-
-    -- local null_ls = require("null-ls")
-    -- local helpers = require("null-ls.helpers")
-    --
-    -- local sca2d = {
-    --   method = null_ls.methods.DIAGNOSTICS,
-    --   filetypes = { "openscad" },
-    --   generator = null_ls.generator({
-    --     command = "sca2d",
-    --     args = { "$FILENAME" },
-    --     from_stderr = false,
-    --     to_stdin = true,
-    --     format = "line",
-    --     check_exit_code = function(code)
-    --       return code <= 1
-    --     end,
-    --     on_output = helpers.diagnostics.from_pattern(
-    --       [[[^:]+:(%d+):(%d+): (%w)%d+: (.*)]], {"row", "col", "severity", "message"}, {
-    --         severities = {
-    --           F = helpers.diagnostics.severities["error"],
-    --           E = helpers.diagnostics.severities["error"],
-    --           W = helpers.diagnostics.severities["warning"],
-    --           D = helpers.diagnostics.severities["warning"],
-    --           I = helpers.diagnostics.severities["info"],
-    --         },
-    --     }),
-    --   }),
-    -- }
-
-    -- null_ls.register(sca2d)
   '';
 
   plugins.which-key.enable = true;
