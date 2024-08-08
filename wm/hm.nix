ba3cdcd0421b4d7b7e654009ebf85e81f749a5a1
traxys <quentin@familleboyer.net>
2024-08-08 22:51:45 +0200

    gui: Move terminal settings from wm

--- wm/hm.nix
+++ wm/hm.nix
@@ -58,54 +58,6 @@
     name = "Adwaita";
   };
 
-  terminal = {
-    enable = true;
-    kind = "foot";
-
-    colors = {
-      background = "1a1b26";
-      foreground = "c0caf5";
-
-      black = {
-        normal = "15161e";
-        bright = "414868";
-      };
-      red = {
-        normal = "f7768e";
-      };
-      green = {
-        normal = "9ece6a";
-      };
-      yellow = {
-        normal = "e0af68";
-      };
-      blue = {
-        normal = "7aa2f7";
-      };
-      magenta = {
-        normal = "bb9af7";
-      };
-      cyan = {
-        normal = "7dcfff";
-      };
-      white = {
-        normal = "a9b1d6";
-        bright = "c0caf5";
-      };
-
-      urls = "73daca";
-
-      selection = {
-        foreground = "c0caf5";
-        background = "33467c";
-      };
-    };
-    font = {
-      family = "Hack Nerd Font Mono";
-      size = lib.mkDefault 10;
-    };
-  };
-
   programs.foot.settings.colors."16" = "ff9e64";
   programs.foot.settings.colors."17" = "db4b4b";
 
