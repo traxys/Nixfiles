091ff933a20760d16c150fc885c0f6c7c81e31bf
Quentin Boyer <quentin+dev@familleboyer.net>
2024-08-29 00:18:53 +0200

    hostconfig/gandalf: Encrypt swap

--- hostconfig/gandalf/hardware-configuration.nix
+++ hostconfig/gandalf/hardware-configuration.nix
@@ -27,10 +27,6 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/6993932f-5b29-4207-915a-2f185ec9f485"; }
-    ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
