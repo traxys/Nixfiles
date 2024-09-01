47d359fea960b666cb79b5a6dc196525103a37ff
Quentin Boyer <quentin+dev@familleboyer.net>
2024-09-01 14:16:11 +0200

    wm: Add regreet

--- wm/nixos.nix
+++ wm/nixos.nix
@@ -15,4 +15,34 @@
       xdg-desktop-portal-gtk
     ];
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      cage = pkgs.writeShellScriptBin "cage" ''
+        export XKB_DEFAULT_LAYOUT=us
+        export XKB_DEFAULT_VARIANT=dvp
+        exec ${self.lib.getExe super.cage} "$@"
+      '';
+    })
+  ];
+
+  services.displayManager.sessionPackages = with pkgs; [
+    sway
+  ];
+
+  programs.regreet = {
+    enable = true;
+
+    theme.package = pkgs.canta-theme;
+
+    settings = {
+      background.path = pkgs.fetchurl {
+        url = "https://lesmondaines.com/wp-content/uploads/2018/07/lac-crozet-rando-2.jpg";
+        hash = "sha256-s35RoLnAyGhDNJh5+qbDEqCM7gF3U2Tyzx4X7jzhT70=";
+      };
+      GTK = {
+        application_prefer_dark_theme = true;
+      };
+    };
+  };
 }
