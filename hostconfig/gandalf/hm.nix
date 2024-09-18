bb78e185429c42f9f6f5fb818cb75d4a60ba1744
Quentin Boyer <quentin+dev@familleboyer.net>
2024-09-18 20:49:15 +0200

    hostconfig/gandalf: Mount boh-articles

--- hostconfig/gandalf/hm.nix
+++ hostconfig/gandalf/hm.nix
@@ -1,8 +1,40 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   programs.git = {
     userName = "Quentin Boyer";
     userEmail = config.extraInfo.email;
+  };
+
+  systemd.user.services = {
+    "rclone-boh-articles" = {
+      Unit = {
+        Description = "rclone: BoH website articles";
+        Documentation = "man:rclone(1)";
+        After = "network-online.target";
+        Wants = "network-online.target";
+      };
+      Service = {
+        Type = "notify";
+        ExecStartPre = "-${lib.getExe' pkgs.coreutils "mkdir"} -p %h/mnt/boh-articles";
+        ExecStart = ''
+          ${lib.getExe pkgs.rclone} \
+            --config=%h/.config/rclone/rclone.conf \
+            --log-level INFO \
+            mount drive:Traxys %h/mnt/boh-articles \
+            --drive-shared-with-me
+        '';
+        ExecStop = "/run/wrappers/bin/fusermount -u %h/mnt/boh-articles";
+        Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
   };
 
   traxys.waybar.modules.battery.enable = true;
