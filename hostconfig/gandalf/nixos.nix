d9cff860409d81b890d7eac7f1d7c10bb0c82f01
Quentin Boyer <quentin+dev@familleboyer.net>
2025-11-08 12:45:55 +0100

    gandalf: Adapt to pulseaudio changes

--- hostconfig/gandalf/nixos.nix
+++ hostconfig/gandalf/nixos.nix
@@ -63,7 +63,7 @@ in
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
