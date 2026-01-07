b6d668c8b28a0db7649a2e1b22646da12a007a29
traxys <quentin@familleboyer.net>
2026-01-07 13:53:49 +0100

    ZeNixComputa: Fix ckb-next

--- hostconfig/ZeNixComputa/nixos.nix
+++ hostconfig/ZeNixComputa/nixos.nix
@@ -113,7 +113,12 @@
     };
   };
 
-  hardware.ckb-next.enable = true;
+  hardware.ckb-next = {
+    enable = true;
+    package = pkgs.ckb-next.overrideAttrs (old: {
+      cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DUSE_DBUS_MENU=0" ];
+    });
+  };
 
   services.postgresql = {
     package = pkgs.postgresql_16;
