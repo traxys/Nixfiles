2a8ddb94293e1f75747169ad28903bfbb163b3d3
Quentin Boyer <quentin+dev@familleboyer.net>
2026-01-16 23:28:37 +0100

    gandalf: Deploy a ArchiveTeam warrior dashboard

--- hostconfig/gandalf/nixos.nix
+++ hostconfig/gandalf/nixos.nix
@@ -99,7 +99,26 @@ in
     ];
   };
 
-  networking.firewall.allowedTCPPorts = [ 9080 4713 ];
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."atw-dash" = {
+    listen = [
+      {
+        addr = "127.0.0.1";
+        port = 63542;
+      }
+    ];
+
+    root = pkgs.fetchgit {
+      url = "https://forge.hackers.town/antijingoist/atw-dashboard.git";
+      rev = "03fa6a4a9ccb18844060e44ba9869b1a80922090";
+      hash = "sha256-RAMRfHV6pB2jgmw/iBr3cj2AsNOQxaT69fk61sgutPQ=";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    9080
+    4713
+  ];
   traxys.wm = "niri";
 
   system.stateVersion = "24.05";
