87da024007dc6b97ba1f9c0c9cb5cf439bf1c888
Quentin Boyer <quentin+dev@familleboyer.net>
2026-06-19 21:15:12 +0200

    nixos: Add fenix cache

---
+++ nixos/cachix/fenix.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://fenix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "fenix.cachix.org-1:ecJhr+RdYEdcVgUkjruiYhjbBloIEGov7bos90cZi0Q="
+      ];
+    };
+  };
+}
